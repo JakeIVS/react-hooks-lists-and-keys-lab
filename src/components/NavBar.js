3c38869fec24987caab1092afc65a74ccaa7c5ff
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-23 12:19:19 -0600

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkList = links.map(link=><a href={`#${link}`} key={link}>{link}</a>)
+  console.log(linkList)
+  return (<nav>
+    {linkList}
+  </nav>);
 }
 
 export default NavBar;
